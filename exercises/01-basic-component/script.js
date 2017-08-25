201647588111b08ff2a3267ea93b1fde2bbe6a1c
Andrés Villanueva <andresvillanueva@MacBook-Pro-de-Apple.local>
2017-08-25 14:12:39 -0300

    feat(01): intro to Vue

---
+++ exercises/01-basic-component/script.js
@@ -0,0 +1,7 @@
+var card = new Vue({
+    el:'#card',
+    data: {
+        title: 'Dinosaurs',
+        content: '<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period.'
+    }
+});
