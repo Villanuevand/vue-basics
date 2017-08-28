78b490f94a74977f323886f6d9dd5d4247b2af5f
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-08-28 07:40:59 -0300

    feat(Lesson06): style binding

---
+++ exercises/06-update-attr-class-styles/script.js
@@ -0,0 +1,22 @@
+var card = new Vue({
+    el:'#card',
+    data: {
+        title: 'Style Binding',
+        sizeToggle: false,
+        isRounded: false,
+        disabled: false,
+        backgroundColor: '#CCCCCC',
+        fontColor: '#000000',
+        range:50
+    },
+    computed: {
+        styles: function () {
+            return {
+                color: this.fontColor,
+                background: this.backgroundColor,
+                'margin-left': this.range+"%"
+              }
+        }
+    }
+
+});
---
+++ lessons/06-update-attr-class-styles.md
@@ -0,0 +1,3 @@
+# Update Attributes, Classes and Styles in Vue.js with v-bind
+
+Use v-bind:class and v-bind:style to compute html classes and inline styles from component data. Vue.js will automatically add vendor prefixes when using v-bind:style.
