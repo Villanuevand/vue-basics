28dbdda4512a08e9bddb3faca72dabc98909f327
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-08-28 07:54:12 -0300

    feat(Lesson07): Conditional elements dom render

---
+++ exercises/07-conditionally-render-dom-elements/script.js
@@ -0,0 +1,31 @@
+var card = new Vue({
+    el:'#card',
+    data: {
+        title: 'Epic Dinosaur Counter',
+        input: '',
+        speciesUpdated: 0,
+        dinosUpdated: 0,
+        buttonText: "Add Dinosaur",
+        items: [
+            { text: "Tyrannosaurus", quantity: 5 },
+            { text: "Triceratops", quantity: 3 },
+            { text: "Stegosaurus", quantity: 2 }
+          ]
+    },
+    methods: {
+        addItem: function (e) {
+            e.preventDefault();
+            if(!this.input) return '';
+            this.items.push({text:this.input,quantity:1});
+            this.input = '';
+        },
+        removeItem: function (item) {
+            this.items.splice(item, 1);
+        }
+    },
+    computed: {
+        buttonDisabled: function() {
+            return this.input == "";
+          }
+    }
+});
---
+++ lessons/07-conditionally-render-dom-elements.md
@@ -0,0 +1,3 @@
+# Conditionally Render DOM Elements in Vue.js
+
+You can use v-if and v-else to display content based on assertions on your data. Similarly, v-show can be used to render the content, but hide it with css. v-if can also be used on an invisible wrapper <template> element.
